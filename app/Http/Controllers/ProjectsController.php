2d1dbf3be0b5cb85891fc4db53e578761d322058
Iqbal <dibaliqaja@gmail.com>
2020-05-28 17:20:57 +0700

    #4 - Model tests

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -14,6 +14,11 @@ class ProjectsController extends Controller
         return view('projects.index', compact('projects'));
     }
 
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
     public function store()
     {
         $attributes = request()->validate([
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 });
 
 Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/{project}', 'ProjectsController@show');
 Route::post('/projects', 'ProjectsController@store');
