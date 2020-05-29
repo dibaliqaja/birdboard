9e7c4c9567f4040ee54ec5e545d9cc013fbd7f62
Iqbal <dibaliqaja@gmail.com>
2020-05-30 05:30:09 +0700

    #10 - Grid spacing and card tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container mx-auto">
                 <div class="flex justify-between items-center py-2">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        <img src="/images/logo.svg" alt="Birdboard">
                     </a>
 
                     <div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,20 +2,31 @@
 
 @section('content')
 
-    <div class="flex items-center mb-3">
-        <a href="/projects/create">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h3 class="text-grey">My Projects</h3>
+            <a href="/projects/create"
+               class="bg-blue text-white text-sm no-underline rounded-lg py-3 px-5"
+               style="box-shadow: 0 2px 7px 0 #b0eaff">
+               Add Project
+            </a>
+        </div>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
-                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h3>
 
-                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
-    </div>
+    </main>
 
 @endsection
