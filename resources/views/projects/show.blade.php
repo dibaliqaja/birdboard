2f8912fbea746df5734a29fc00c28a791cea3514
Iqbal <dibaliqaja@gmail.com>
2020-05-30 08:52:31 +0700

    #13 - Task UI updates: Part 1

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -19,19 +19,25 @@
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-grey text-lg font-normal mb-3">Tasks</h2>
+                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
 
                     @foreach ($project->tasks as $task)
                         <div class="bg-white p-5 rounded-lg shadow mb-3">{{ $task->body }}</div>
                     @endforeach
+                    <div class="bg-white p-5 rounded-lg shadow mb-3">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                            @csrf
+                            <input type="text" name="body" class="w-full" placeholder="Add a new task...">
+                        </form>
+                    </div>
                 </div>
 
                 <div>
-                    <h2 class="text-grey text-lg font-normal mb-3   ">General Notes</h2>
+                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
                     <textarea class="bg-white p-5 rounded-lg shadow w-full" style="min-height: 200px">Lorem ipsum</textarea>
                 </div>
             </div>
-            <div class="lg:w-1/4 px-3">
+            <div class="lg:w-1/4 px-3 lg:py-8">
                 @include('projects.card')
             </div>
         </div>
