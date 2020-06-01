f9f792d522274d061e087473106512050862c285
Iqbal <dibaliqaja@gmail.com>
2020-06-01 21:23:02 +0700

    #26 - Recording model changes

--- app/Project.php
+++ app/Project.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
     protected $guarded = [];
+
+    public $old = [];
 
     public function path()
     {
@@ -28,9 +31,27 @@ class Project extends Model
         return $this->tasks()->create(compact('body'));
     }
 
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
     public function recordActivity($description)
     {
-        $this->activity()->create(compact('description'));
+        $this->activity()->create([
+            'description' => $description,
+            'changes' => $this->activityChanges($description)
+        ]);
+    }
+
+    protected function activityChanges($description)
+    {
+        if ($description == 'updated') {
+            return [
+                'before' => array_except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
+                'after' => array_except($this->getChanges(), 'updated_at')
+            ];
+        }
     }
 
     public function activity()
